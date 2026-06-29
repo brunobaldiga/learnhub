98e6fce798416a9aaf7acd5d81675396109919b2
Bruno Baldiga <brunobaldiga1@gmail.com>
2026-06-29 10:31:18 -0300

    hotfix: delete video status code change

--- src/main/java/org/example/learnhub/LearnhubApplication.java
+++ src/main/java/org/example/learnhub/LearnhubApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearnhubApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LearnhubApplication.class, args);
     }
-
 }
